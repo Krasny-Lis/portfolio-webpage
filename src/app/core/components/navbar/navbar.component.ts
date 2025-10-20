1b98019275f4df1cd20384249bf267f2d9635d58
KrasnyLis <akceleratorr@gmail.com>
2025-10-20 06:42:49 +0200

    improved lang toogle

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -15,7 +15,7 @@ import { Language } from '../../services/translation.data';
   imports: [RouterLink, RouterLinkActive, AsyncPipe, NgFor, NgIf, DrawerDirective],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
   private themeService = inject(ThemeService);
@@ -23,20 +23,18 @@ export class NavbarComponent {
   private translations = inject(TranslationService);
 
   readonly theme = this.themeService.theme;
-  readonly links$ = this.content.getSocialLinks().pipe(
-    map((links) => links.filter((link) => link.url.startsWith('http')))
-  );
+  readonly links$ = this.content
+    .getSocialLinks()
+    .pipe(map((links) => links.filter((link) => link.url.startsWith('http'))));
   readonly t = this.translations.translations;
   readonly language = this.translations.language;
   readonly availableLanguages: Language[] = ['en', 'pl'];
   readonly languageIndex = computed(() =>
-    Math.max(0, this.availableLanguages.indexOf(this.language()))
+    Math.max(0, this.availableLanguages.indexOf(this.language())),
   );
 
   readonly modeLabel = computed(() =>
-    this.theme() === 'dark'
-      ? this.t().navbar.themeToggle.light
-      : this.t().navbar.themeToggle.dark
+    this.theme() === 'dark' ? this.t().navbar.themeToggle.light : this.t().navbar.themeToggle.dark,
   );
 
   toggleTheme(): void {
@@ -44,6 +42,11 @@ export class NavbarComponent {
   }
 
   setLanguage(lang: Language): void {
+    if (this.language() === lang) {
+      this.translations.toggleLanguage();
+      return;
+    }
+
     this.translations.setLanguage(lang);
   }
 }
